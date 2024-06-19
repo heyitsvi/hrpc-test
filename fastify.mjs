863e72b32d858c580a588ab3dacb9996d971326f
Vivek <vivek99sanjeetsingh@gmail.com>
2024-06-19 12:18:51 +0530

    add hyperbee to store logs

--- fastify.mjs
+++ fastify.mjs
@@ -1,15 +1,42 @@
 import RPC from '@hyperswarm/rpc';
 import Fastify from 'fastify';
 import { serverPublicKey } from './hrpc.mjs';
+import Hyperbee from 'hyperbee';
+import Hypercore from 'hypercore';
 
 const fastify = Fastify({
     logger: false
   })
 
+const core = new Hypercore('./mydb');
+await core.ready();
+
+// Create a Hyperbee instance
+const db = new Hyperbee(core, {
+  keyEncoding: 'utf-8',
+  valueEncoding: 'json'
+});
+
+// Endpoint to get logs
+fastify.get('/log', async (req, res) => {
+  const stream = db.createReadStream();
+  const logs = [];
+  for await (const { key, value } of stream) {  
+    logs.push({ name: key, count: value });
+  }
+  res.send(logs);
+});
+
+// Log requests and update word counts
+async function logRequest(key) {
+  const countEntry = await db.get(key);
+  const count = countEntry ? countEntry.value : 0;
+  await db.put(key, count + 1);
+}
+
 fastify.get('/', function (request, reply) {
     reply.send({ hello: 'world' })
 })
-
 
 const rpc = new RPC();
 const client = rpc.connect(serverPublicKey);
@@ -22,6 +49,7 @@ fastify.post('/hello', async (request, reply) => {
       const reqBuffer = Buffer.from(JSON.stringify({ name }));
       const responseBuffer = await client.request('hello', reqBuffer);
       const response = responseBuffer.toString(); 
+      await logRequest(name);
       reply.send(response); // Send the response from Backend 1 back to the client
       // console.log(response);
     } catch (err) {
